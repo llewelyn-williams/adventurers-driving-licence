0f5d7de5f60a653f2667a8246f232dcd37058e3b
llewelyn-williams <llewelyn.williams@googlemail.com>
2021-09-14 14:53:45 +0000

    test: Remove JSON file and use JS object instead
    While separation would have been nice, the process of linking them back
    together for use was an overcomplication.

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,7 @@ function moveCard(){
     let lpos = card1.style.left; //assign the starting left to a variable
     let myNum = Number(card1.style.left.slice(0, lpos.length - 2)); //get just the number part from the "..px" left value
     card1.style.left = (myNum + 12) + "px"; //add 30 to it and then append the "px" back on and update the left type to be that.
-    console.log((myNum + 12) + "px"); // console log the updating values
+    //console.log((myNum + 12) + "px"); // console log the updating values
 
     // stop animating
     if (myNum >= 0) {
@@ -17,6 +17,129 @@ function moveCard(){
         card1.style.left = "0px"; // set the card back to its intended end position.
     };
 }
+
+
+
+// Put content from massive object onto the page
+
+//Question Data
+let exam = {
+    "question1": {
+      "questionText": "Question 1",
+      "answers": {
+        "answer1": {
+          "answerText": "Q1 Answer One",
+          "answerStat": 5,
+          "answerResponse": "Q1 A1 Response"
+        },
+        "answer2": {
+          "answerText": "Q1 Answer Two",
+          "answerStat": 5,
+          "answerResponse": "Q1 A2 Response"
+        },
+        "answer3": {
+          "answerText": "Q1 Answer Three",
+          "answerStat": 5,
+          "answerResponse": "Q1 A3 Response"
+        }
+      }
+    },
+    "question2": {
+      "questionText": "Question 2",
+      "answers": {
+        "answer1": {
+          "answerText": "Q2 Answer One",
+          "answerStat": 5,
+          "answerResponse": "Q2 A1 Response"
+        },
+        "answer2": {
+          "answerText": "Q2 Answer Two",
+          "answerStat": 5,
+          "answerResponse": "Q2 A2 Response"
+        },
+        "answer3": {
+          "answerText": "Q2 Answer Three",
+          "answerStat": 5,
+          "answerResponse": "Q2 A3 Response"
+        }
+      }
+    },
+    "question3": {
+      "questionText": "Question 2",
+      "answers": {
+        "answer1": {
+          "answerText": "Q3 Answer One",
+          "answerStat": 5,
+          "answerResponse": "Q3 A1 Response"
+        },
+        "answer2": {
+          "answerText": "Q3 Answer Two",
+          "answerStat": 5,
+          "answerResponse": "Q3 A2 Response"
+        },
+        "answer3": {
+          "answerText": "Q3 Answer Three",
+          "answerStat": 5,
+          "answerResponse": "Q3 A3 Response"
+        }
+      }
+    },
+    "question4": {
+      "questionText": "Question 4",
+      "answers": {
+        "answer1": {
+          "answerText": "Q4 Answer One",
+          "answerStat": 5,
+          "answerResponse": "Q4 A1 Response"
+        },
+        "answer2": {
+          "answerText": "Q4 Answer Two",
+          "answerStat": 5,
+          "answerResponse": "Q4 A2 Response"
+        },
+        "answer3": {
+          "answerText": "Q4 Answer Three",
+          "answerStat": 5,
+          "answerResponse": "Q4 A3 Response"
+        }
+      }
+    },
+    "question5": {
+      "questionText": "Question 5",
+      "answers": {
+        "answer1": {
+          "answerText": "Q5 Answer One",
+          "answerStat": 5,
+          "answerResponse": "Q5 A1 Response"
+        },
+        "answer2": {
+          "answerText": "Q5 Answer Two",
+          "answerStat": 5,
+          "answerResponse": "Q5 A2 Response"
+        },
+        "answer3": {
+          "answerText": "Q5 Answer Three",
+          "answerStat": 5,
+          "answerResponse": "Q5 A3 Response"
+        }
+      }
+    }
+  };
+
+//Variables for Page elements
+let card2 = document.getElementById("card2"); //the second div
+let card3 = document.getElementById("card3"); //the third div
+
+
+//https://stackoverflow.com/questions/19706046/how-to-read-an-external-local-json-file-in-javascript/34579496
+//didn't use, went for object instead
+
+//Set the innerHTML of the page elements
+card1.innerHTML = exam.question1.answers.answer1.answerText;
+card2.innerHTML = exam.question1.answers.answer2.answerText;
+card3.innerHTML = exam.question1.answers.answer3.answerText;
+
+
 
 
 
