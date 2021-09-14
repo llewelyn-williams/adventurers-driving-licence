99dc9b5c5cd5e7bb5407068bd8b7b638bb92f799
llewelyn-williams <llewelyn.williams@googlemail.com>
2021-09-14 19:37:57 +0000

    refactor: Change all let variable to const
    There was no need for any of them to not be const.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,13 @@
-let card1 = document.getElementById("card1"); //the first div
+const card1 = document.getElementById("card1"); //the first div
 
 document.getElementById("card1").style.left = - (screen.width/2 + 60) + "px"; // position it nearly completely off to the left
 console.log (document.getElementById("card1").style.left); // check it's position in the console
 
-let cardAnimator = setInterval(moveCard, 1000/120); // this presumabley creates 120fps style fluidity
+const cardAnimator = setInterval(moveCard, 1000/120); // this presumabley creates 120fps style fluidity
 
 function moveCard(){
-    let lpos = card1.style.left; //assign the starting left to a variable
-    let myNum = Number(card1.style.left.slice(0, lpos.length - 2)); //get just the number part from the "..px" left value
+    const lpos = card1.style.left; //assign the starting left to a variable
+    const myNum = Number(card1.style.left.slice(0, lpos.length - 2)); //get just the number part from the "..px" left value
     card1.style.left = (myNum + 12) + "px"; //add 30 to it and then append the "px" back on and update the left type to be that.
     //console.log((myNum + 12) + "px"); // console log the updating values
 
@@ -15,16 +15,16 @@ function moveCard(){
     if (myNum >= 0) {
         clearInterval(cardAnimator); //stop the setInterval
         card1.style.left = "0px"; // set the card back to its intended end position.
-    };
+    }
 };
 
 
 // Put content from massive object onto the page
 
 //Variables for Page elements
-let questionCard = document.getElementById("question-display"); //the question card
-let card2 = document.getElementById("card2"); //the second div
-let card3 = document.getElementById("card3"); //the third div
+const questionCard = document.getElementById("question-display"); //the question card
+const card2 = document.getElementById("card2"); //the second div
+const card3 = document.getElementById("card3"); //the third div
 
 
 //https://stackoverflow.com/questions/19706046/how-to-read-an-external-local-json-file-in-javascript/34579496
@@ -38,21 +38,21 @@ card3.innerHTML = exam.question1.answers.answer3.answerText;
 
 
 //GameState
-let questioning = true;
+const questioning = true;
 
 if (questioning){
     document.getElementById("title").hidden = true;
     document.getElementById("start").hidden = true;
     document.getElementById("options").hidden  = true;
-};
+}
 
-let newgame = false;
+const newgame = false;
 
 if (newgame){
     questionCard.hidden = true;
     card1.hidden = true;
     card2.hidden = true;
     card3.hidden = true;
-};
+}
 
 
